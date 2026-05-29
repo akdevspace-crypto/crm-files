4789cbb238688f540086b1a3899e795ad92bd5c8
akdevspace-crypto <akdevspace@gmail.com>
2026-05-29 22:19:02 +0530

    Add files via upload

---
+++ dist/reset.js
@@ -0,0 +1,8 @@
+"use strict";
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+prisma.agent.updateMany({ data: { status: 'AVAILABLE', activeCalls: 0 } }).then(res => {
+    console.log('Reset agents:', res);
+    prisma.$disconnect();
+});
+//# sourceMappingURL=reset.js.map
